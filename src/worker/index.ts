84e957c90ac3a4a3920d58256f77602127d5a321
Robert Faust <robert.faust@labelzoom.net>
2025-05-21 12:59:24 -0500

    Adding CORS middleware

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,30 @@
 import { Hono } from "hono";
+import { cors } from 'hono/cors';
+
 const app = new Hono<{ Bindings: Env }>();
+
+app.use("/api/*", async (c, next) => {
+    const corsMiddleware = cors({
+        origin: c.env.LZ_ALLOWED_ORIGINS,
+        allowHeaders: [],
+        allowMethods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+
+        /* CORS-safelisted response headers:
+        * - Cache-Control
+        * - Content-Language
+        * - Content-Length
+        * - Content-Type
+        * - Expires
+        * - Last-Modified
+        * - Pragma
+        */
+        exposeHeaders: ['Retry-After', 'X-LZ-Request-ID'],
+
+        maxAge: 86400,
+        credentials: true,
+    });
+    return corsMiddleware(c, next);
+});
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 
