b6d1f874372f4b930878d7758d744ef8928d2efb
Robert Faust <robert.faust@labelzoom.net>
2025-12-19 09:47:26 -0600

    Adding JUnit test report

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 export default defineWorkersConfig({
   test: {
     globals: true,
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: './test-results/junit-report.xml',
+    },
     poolOptions: {
       workers: {
         wrangler: { configPath: './wrangler.jsonc' },
